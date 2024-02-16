84864826b3f42b9e82904abb8b06dd8ab1a03f9b
pedradasdev <160197897+pedradasdev@users.noreply.github.com>
2024-02-15 23:35:26 -0300

    upload de projeto
    projeto completo de acordo com especificaçõea do cliente

---
+++ ID168 Starter IT m4_supportmaterial_ Class3.c
@@ -0,0 +1,125 @@
+#include <stdio.h> //biblioteca de comunicação com o usuário
+#include <stdlib.h> //biblioteca de alocação de espaço em memória
+#include <locale.h> //biblioteca de alocações de texto por região
+#include <string.h> //biblioteca responsável por cuidar das string
+		
+int registro()
+{
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+	char cargo[40];
+	
+	printf("Digite o CPF a ser cadastrado: ");
+	scanf("%s", cpf);
+	
+	strcpy(arquivo, cpf); //Responsavel por copiar os valores das string
+	
+	FILE *file; // cria o arquivo
+	file = fopen(arquivo, "w"); // cria o arquivo
+	fprintf(file,cpf); // salvo o valor da variavel
+	fclose(file); // fecha o arquivo
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o nome a ser cadastrado: ");
+	scanf("%s",nome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,nome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o sobrenome a ser cadastrado: ");
+	scanf("%s",sobrenome);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,sobrenome);
+	fclose(file);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,",");
+	fclose(file);
+	
+	printf("Digite o cargo a ser cadastrado: ");
+	scanf("%s",cargo);
+	
+	file = fopen(arquivo, "a");
+	fprintf(file,cargo);
+	fclose(file);	
+	
+    system("pause");
+
+}
+
+int consulta()
+{
+	printf("Você escolheu consultar os nomes!\n");
+	system("pause");
+}
+
+int deletar()
+{
+	printf("Você escolheu deletar nomes!\n");
+	system("pause");	
+}
+
+
+	
+int main()
+	{
+	int opcao=0; //Definindo variáveis
+	int laco=1;
+	
+	for(laco=1;laco=1;)
+	{
+
+		system("cls");
+
+		setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
+			
+		printf("### CART�RIO DA EBAC ###\n\n"); //inicio do menu
+		printf("Escolha a op��o desejada no menu!\n\n");
+		printf("\t1 - Registrar nomes\n");
+		printf("\t2 - Consultar nomes\n");
+		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sistema\n"); 
+		printf("Op��o: ");//fim do menu
+				
+		scanf("%d", &opcao); //armazenando a escolha do usuário
+		
+		system("cls");
+	
+		switch(opcao) //inicio da seleção
+		{
+			case 1:
+			registro();
+			break;
+			
+			case 2:
+			consulta();
+			break;
+		
+			case 3:
+			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;	
+		
+			default:
+			printf("Essa opção não está disponivel!\n");
+			system("pause");
+			break;
+		} //fim da seleção
+			
+	}	
+}
